9a47039c8e1020a333d7b9023d7e5d7bfd82d1dc
Paulo Aguiar <pauloaguiar@live.com>
2016-01-10 20:58:56 -0500

    added basic js code

---
+++ main.js
@@ -0,0 +1,15 @@
+(function() {
+	var aq_main = function() {
+
+		function setupEvents() {
+			
+		}
+
+		this.init = function() {
+			setupEvents();
+		};
+	};
+
+	var aq_main = new aq_main();
+		aq_main.init();
+})();
